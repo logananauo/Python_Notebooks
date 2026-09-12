f1bb64618d414b8defa36fe8436bd5fd6367e5cf
Logan Anauo <logan.anauo@pitt.edu>
2026-09-12 10:49:47 -0400

    Update label_encoding.py

--- data_preprocessing/label_encoding.py
+++ data_preprocessing/label_encoding.py
@@ -1,17 +1,9 @@
 import pandas as pd
-
 from sklearn.preprocessing import LabelEncoder
 
-# One-hot encoding (most common)
+### one-hot encoding (most common)
+df = pd.get_dummies(df, columns=['region', 'product_type'])
 
-df = pd.get_dummies(
-
-    df, columns=['region', 'product_type']
-
-)
-
-# Label encoding for ordinal features
-
+### label encoding for ordinal features
 le = LabelEncoder()
-
 df['tier_encoded'] = le.fit_transform(df['tier'])
