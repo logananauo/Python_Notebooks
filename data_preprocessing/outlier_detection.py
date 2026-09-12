de08f16efb927926379868b6f4032350993abc44
Logan Anauo <logan.anauo@pitt.edu>
2026-09-12 10:51:10 -0400

    Update outlier_detection.py

--- data_preprocessing/outlier_detection.py
+++ data_preprocessing/outlier_detection.py
@@ -1,21 +1,11 @@
 import pandas as pd
 
 Q1 = df['purchase_amount'].quantile(0.25)
-
 Q3 = df['purchase_amount'].quantile(0.75)
 
 IQR = Q3 - Q1
-
 lower = Q1 - 1.5 * IQR
-
 upper = Q3 + 1.5 * IQR
 
-# Filter or flag (flagging is often safer)
-
-df['is_outlier'] = (
-
-    (df['purchase_amount'] < lower) |
-
-    (df['purchase_amount'] > upper)
-
-).astype(int)
+### filter or flag (flagging is often safer)
+df['is_outlier'] = ((df['purchase_amount'] < lower) | (df['purchase_amount'] > upper)).astype(int)
